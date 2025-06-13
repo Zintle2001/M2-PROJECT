25cea7dda5d6b67fe7932051db869876845065a0
sthembiile37 <Sithembilekubheka37@gmail.com>
2025-06-13 11:53:57 +0200

    Update Form2.cs

--- Appointment system/Form2.cs
+++ Appointment system/Form2.cs
@@ -23,5 +23,44 @@ namespace Appointment_system
             this.patientTableAdapter.Fill(this.wstGrp25DataSet.Patient);
 
         }
+        
+        private void dgvPatient_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+                int rowIndex = e.RowIndex;
+                int PatientID = Convert.ToInt32(dgvPatient.Rows[rowIndex].Cells[0].Value);
+                 //int currentPatientID = selectedPatientID;
+                txtPatientID.Text = PatientID.ToString();
+
+                string name = dgvPatient.Rows[rowIndex].Cells[1].Value.ToString();
+                txtName.Text = name;
+
+
+                string surname = dgvPatient.Rows[rowIndex].Cells[2].Value.ToString();
+                txtSurname.Text = surname;
+
+                string email = dgvPatient.Rows[rowIndex].Cells[4].Value.ToString();
+                txtEmail.Text = email;
+
+                string phoneNumber = dgvPatient.Rows[rowIndex].Cells[6].Value.ToString();
+                txtPhone.Text = phoneNumber;
+
+                string streetName = dgvPatient.Rows[rowIndex].Cells[7].Value.ToString();
+                txtStreetName.Text = streetName;
+
+                string suburb = dgvPatient.Rows[rowIndex].Cells[10].Value.ToString();
+                txtSuburb.Text = suburb;
+
+                string city = dgvPatient.Rows[rowIndex].Cells[9].Value.ToString();
+                txtCity.Text = city;
+
+                int code = Convert.ToInt32(dgvPatient.Rows[rowIndex].Cells[8].Value);
+                txtPostalCode.Text = code.ToString();
+            }
+
+        
+        private void OKbutton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Patient added!");
+        }
     }
 }
